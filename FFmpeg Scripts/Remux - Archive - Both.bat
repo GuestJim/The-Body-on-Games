f823bc72e7ad4e9e455f1741a08258d19136a629
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-04-23 17:42:34 -0400

    Update Remux - Archive - Both.bat

--- FFmpeg Scripts/Remux - Archive - Both.bat
+++ FFmpeg Scripts/Remux - Archive - Both.bat
@@ -2,14 +2,11 @@
 ::echo "%~1"
 TITLE Remux TBOG Archive
 
-::this does work, but I don't know to what end. Just handy. Need to use VLC, but it will play both video streams concurrently and allows the audio streams to be selected.
-
-
-::this will set the name to be the name of the folder the files are in
 for %%* in (.) do set name=%%~nx*
 echo %name%
+::	this will set the name to be the name of the folder the files are in
 
-::this third input puts an empty Mix audio track in the spot it will be in the final archive. This allows me to use the same Demux command for both archive types
+::	this third input puts an empty Mix audio track in the spot it will be in the final archive. This allows me to use the same Demux command for both archive types
 
 ffmpeg -i "%~1" -i "%~2" -i "%~3" -f lavfi -i anullsrc=cl=stereo:r=44100 -shortest -c copy ^
 -map 0:v -map 1:v -map 2:v -map 3:a -map 0:a:1 -map 0:a:2 -map 1:a:1? -map 1:a:2? -map 2:a:1? -map 2:a:2? ^
