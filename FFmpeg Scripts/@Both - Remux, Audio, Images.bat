4846df7823ec35f93bbe3da2e0ff96f26a8f88ec
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-06-02 11:20:19 -0400

    Update @Both - Remux, Audio, Images.bat

--- FFmpeg Scripts/@Both - Remux, Audio, Images.bat
+++ FFmpeg Scripts/@Both - Remux, Audio, Images.bat
@@ -6,9 +6,6 @@ set batloc=C:\Users\Jim\Desktop\FFmpeg Offload
 ::Desktop
 set batloc=E:\Users\Jim\My Videos\FFMPEG Batch files\TBOG
 
-
-set type=Remux - Archive
-
 start CMD /c call "%batloc%\Remux - Archive - Both.bat" "%~1" "%~2" "%~3"
 ::	Makes the Archive of the files
 
@@ -16,4 +13,4 @@ start CMD /c call "%batloc%\LoudNorm - Mic.bat" "%~1"
 ::	Processes the microhpone audio
 
 start CMD /c call "%batloc%\Screenshots - 1 Sec.bat" "%~2"
-::	Processes the microhpone audio
+::	grabs first and last 30 frames fromthe BPM graph to cut ends off CSV data
