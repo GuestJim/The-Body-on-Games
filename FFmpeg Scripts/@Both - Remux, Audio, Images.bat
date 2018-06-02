eb43b1fc3669cb188cd0b2b9e20951dda2df8458
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-06-02 11:19:41 -0400

    Update @Both - Remux, Audio, Images.bat

--- FFmpeg Scripts/@Both - Remux, Audio, Images.bat
+++ FFmpeg Scripts/@Both - Remux, Audio, Images.bat
@@ -7,8 +7,6 @@ set batloc=C:\Users\Jim\Desktop\FFmpeg Offload
 set batloc=E:\Users\Jim\My Videos\FFMPEG Batch files\TBOG
 ::	my two computers have files in different locations
 
-set type=Remux - Archive
-
 start CMD /c call "%batloc%\Remux - Archive - Both.bat" "%~1" "%~2" "%~3"
 ::	Makes the Archive of the files
 
@@ -16,4 +14,4 @@ start CMD /c call "%batloc%\LoudNorm - Mic.bat" "%~1"
 ::	Processes the microhpone audio
 
 start CMD /c call "%batloc%\Screenshots - 1 Sec.bat" "%~2"
-::	Processes the microhpone audio
+::	grabs first and last 30 frames fromthe BPM graph to cut ends off CSV data
